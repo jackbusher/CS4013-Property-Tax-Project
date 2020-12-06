630d6831d0b78a4170129364cf6ba5a478000d49
Eoin Chedzey <51025347+Chedz@users.noreply.github.com>
2020-12-06 05:11:56 +0000

    Updated UI to display entire system functionality
    The previous UI was only displaying how to register a property. GUI has been fully redesigned to now have a home screen on the left side of the screen. The selected category of either "Register a Property, Manage Properties, View Payments or Department of Environment" will then show up on the right side. Users will have the ability to switch between different modes easily with the UI overhaul.

--- GUI.java
+++ GUI.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI {
+	 JPanel container = new JPanel(); //container panel to fit into frame, holds both panels
+     JFrame frame = new JFrame(); //frame for entire GUI
+     JPanel panel = new JPanel(); //RHS of screen, holds content of one of 4 chosen utilities
     
     
     PropertyManagementInterface propertyManagery;
@@ -11,21 +14,94 @@ public class GUI {
     public GUI() {
         
         this.propertyManagery  = new PropertyManagementImpl();
-        JFrame frame = new JFrame();     
-
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(200,200,100,100));
-        panel.setLayout(new GridLayout(0,1));
+        home(); //home screen is the LHS of screen with 4 different utilities
+        }
         
         
+    public void home(){
+        	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  
+        
 
-        frame.add(panel, BorderLayout.CENTER);
+        container.setLayout(new BoxLayout(container, BoxLayout.X_AXIS));
+        
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        //JPanel panel2 = new JPanel();
+        
+        
+        container.add(panel1);
+        //container.add(panel2);
+        
+        frame.add(container, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("GUI");
-        frame.pack();
+       // frame.pack();
+        frame.setSize((int)screenSize.getWidth()-200,(int)screenSize.getHeight()-200);
+        frame.setResizable(false);
         frame.setVisible(true);
+        frame.revalidate();
+        frame.repaint();
+
         
+        //create and add jbuttons to panel1
+        JButton r = new JButton("Register a Property");
+        JButton m = new JButton("Manage Properties");
+        JButton v = new JButton("View Payments");
+        JButton doe = new JButton("Department of Environnment Management");
+        
+        r.addActionListener(new ActionListener() { 
+        	  public void actionPerformed(ActionEvent e) { 
+        	    register();
+        	  } 
+        	} );
+        m.addActionListener(new ActionListener() { 
+        	  public void actionPerformed(ActionEvent e) { 
+        	    manage();
+        	  } 
+        	} );
+        v.addActionListener(new ActionListener() { 
+        	  public void actionPerformed(ActionEvent e) { 
+        	    view();
+        	  } 
+        	} );
+        doe.addActionListener(new ActionListener() { 
+        	  public void actionPerformed(ActionEvent e) { 
+        	    envMan();
+        	  } 
+        	} );
+        
+        panel1.add(Box.createVerticalGlue());
+        panel1.add(r);
+        panel1.add(Box.createVerticalGlue());
+        panel1.add(m);
+        panel1.add(Box.createVerticalGlue());
+        panel1.add(v);
+        panel1.add(Box.createVerticalGlue());
+        panel1.add(doe);
+        panel1.add(Box.createVerticalGlue());
+        
+        
+        panel1.setVisible(true);
+        panel1.revalidate();
+        panel1.repaint();
+    }
+        
+        
+        
+   public void register() {
+        	System.out.println("Register Property has been called");
         //JLabel label = new JLabel("Enter infomation");
+        container.remove(panel);
+        panel.removeAll();
+
+        //JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(6,2)); // set rhs panel layout
+        container.add(panel);
+        frame.add(container);
+        container.revalidate();
+        container.repaint();
+        	
         JLabel label2 = new JLabel("<html><span style='font-size:20px'>"+"Owners"+"</span></html>");
         JLabel label3 = new JLabel("<html><span style='font-size:20px'>"+"Value"+"</span></html>");
         JLabel label4 = new JLabel("<html><span style='font-size:20px'>"+"Eircode"+"</span></html>");
@@ -74,9 +150,60 @@ public class GUI {
     });
         
 
-        panel.setLayout(new GridLayout(6,2));
 
     }
+        
+    public void manage() { 
+    	 
+    	 System.out.println("Manage Properties has been called");
+         container.remove(panel);
+         panel.removeAll();
+
+         container.add(panel);
+         frame.add(container);
+         container.revalidate();
+         container.repaint();
+         
+         JLabel c = new JLabel("Property List");	//placeholder content
+         panel.add(c);
+    	 
+     }
+        
+     public void view() {
+    	 
+    	 System.out.println("View Payemnts has been called");
+         container.remove(panel);
+         panel.removeAll();
+
+         container.add(panel);
+         frame.add(container);
+         container.revalidate();
+         container.repaint();
+         
+         JLabel c = new JLabel("Payment List");	//placeholder content
+         panel.add(c);
+    	 
+     }
+
+     public void envMan() {
+    	 
+    	 System.out.println("Manage DOE has been called");
+         container.remove(panel);
+         panel.removeAll();
+
+         container.add(panel);
+         frame.add(container);
+         container.revalidate();
+         container.repaint();
+         
+         JLabel c = new JLabel("List of all properties/owners and way to sort them");	//placeholder content
+         panel.add(c);
+	 
+     }
+
+        
+        
+        
         public static void main(String[] args) {
             new GUI();
 
