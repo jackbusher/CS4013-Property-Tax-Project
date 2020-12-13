9527e09165802ab4e359cc6b5675f9b5123fe217
DeanKelly751 <62704110+DeanKelly751@users.noreply.github.com>
2020-12-13 12:09:38 +0000

    Add files via upload

--- Tax.java
+++ Tax.java
@@ -5,20 +5,28 @@ public class Tax extends Property {
     int[] rates = { 100, 80, 60, 50, 25 };
     double accumTax;
     public static ArrayList<Tax> taxlist = new ArrayList<Tax>();
-        public Tax(String owners, String address, String eircode, int value, String locationCategory, char ppr) {
-            super(owners,address,eircode,value,locationCategory,ppr);
-        }
-        
-public double getMarketValueTax () {
+    
+    public Tax(){
+    }
+    
+    public Tax(String owners, String address, String eircode, int value, String locationCategory, char ppr) {
+        super(owners,address,eircode,value,locationCategory,ppr);
+    }
+
+    public double getMarketValueTax () {
+        return getMarketValueTax(value);
+    }
+    
+    private double getMarketValueTax (int val) {
         double taxrate;
 
-        if(value < 150000) {
+        if(val < 150000) {
             taxrate = 0;
         }
-        else if(value < 400000 ) {
+        else if(val < 400000 ) {
             taxrate = .01;
         }
-        else if(value < 650000) {
+        else if(val < 650000) {
             taxrate = .02;
         }
         else {
@@ -26,11 +34,15 @@ public double getMarketValueTax () {
         }
         return taxrate;
     }
-    
+
     public double getLocationTax() {
+        return getLocationTax(locationCategory);
+    }
+    
+    private double getLocationTax(String locationCat) {
         int locationTax=0;
         for(int i=0;i<locations.length;i++) {
-            if(locations[i].contentEquals( locationCategory)) {
+            if(locations[i].contentEquals( locationCat)) {
                 locationTax = rates[i];
                 break;
             }
@@ -39,9 +51,9 @@ public double getMarketValueTax () {
     }
 
     public double accumTax(boolean sub, double subbedValue) {
-    	if(sub) {
-    		return subbedValue;
-    	}
+        if(sub) {
+            return subbedValue;
+        }
         this.accumTax = 100;
         accumTax = (getLocationTax() + value*getMarketValueTax());
         if(getPPR() == 'N') {
@@ -50,9 +62,17 @@ public double getMarketValueTax () {
 
         return accumTax;
     }
-    
-    public static void addTaxList(Tax taxProp){
-    	taxlist.add(taxProp);
-    }
-    }
 
+    public static void addTaxList(Tax taxProp){
+        taxlist.add(taxProp);
+    }
+    
+    public double getTaxForProperty(Property property){
+        double tax = 100;
+        double valueTax = getMarketValueTax(property.getValue());
+        double locationTax = getLocationTax(property.locationCategory());
+        //An additional flat charge of €100 if the property is not the principle private residence of the owner.
+        //Apply a 7% penalty, compounded for each year that a property tax is unpaid
+        return (tax + valueTax + locationTax);
+    }
+}
